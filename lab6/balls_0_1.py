4128f9547668e1e8be433e22c055d23c457faa75
Muravev Matvei <mmm2sp@gmail.com>
2021-10-12 12:03:13 +0300

    Теперь пользователь может настраивать частоту появления шаров и продолжительность игры. Финальный счет обратно пропорционален продолжительности игры

--- lab6/balls_0_1.py
+++ lab6/balls_0_1.py
@@ -1,11 +1,6 @@
 import pygame
 import pygame.draw as dr
 from random import randint
-pygame.init()
-
-FPS = 30
-SIZE = WIDTH, HEIGHT = 500, 500
-screen = pygame.display.set_mode(SIZE)
 
 def const_colors():
     '''
@@ -13,6 +8,7 @@ def const_colors():
     Функция возвращает список из 7 цветов:
     красного, синего, желтого, зеленого, маджента, цвет морской волны, черный
     '''
+    
     red = (255, 0, 0)
     blue = (0, 0, 255)
     yellow = (255, 255, 0)
@@ -28,89 +24,109 @@ def new_ball():
     Рисует новый шарик произвольного размера в случайном месте
     Возвращает координаты и радиус шарика
     '''
+    
+    color = COLORS[randint(1, 6)]
     x = randint(0.1 * WIDTH, 0.9 * WIDTH)
     y = randint(0.1 * HEIGHT, 0.9 * HEIGHT)
     r = randint(0.02 * min(SIZE), 0.1 * min(SIZE))
-    color = COLORS[randint(1, 6)]
-    ball(x, y, r, color)
-    return (x, y, r, color)
+    v_x = WIDTH / randint(2, 10) / FPS
+    v_y = HEIGHT / randint(2, 10) / FPS
+    
+    ball(color, x, y, r)
+    
+    return (color, x, y, r, v_x, v_y)
 
-def ball(x, y, r, color):
+def ball(color, x, y, r):
     '''
     Рисует шарик
+    :param color: цвет шара по системе RGB
     :param x: абсцисса центра шара
     :param y: ордината центра шара
     :param r: радиус шара
-    :param color: цвет шара по системе RGB
     '''
+    
     dr.circle(screen, color, (x, y), r)
     
 
-def move(coords, speed):
+def move(coords):
     '''
     Перемещает шарик
+    :param coords: список из цвета, абсциссы, ординаты, радиуса шара
+    и его скоростей по х и у
     '''
-    x, y, r, color = coords
-    v_x, v_y = speed
-
+    
+    color, x, y, r, v_x, v_y = coords
+    
     x += v_x
     y += v_y
-    ball(x, y, r, color)
-
     if ((x+r) >= WIDTH) or (x-r <= 0):
         v_x = -v_x
     if ((y+r) >= HEIGHT) or (y-r <= 0):
         v_y = -v_y
-        
-    return [x, y, r, color], [v_x, v_y]
+
+    ball(color, x, y, r)
+    return [color, x, y, r, v_x, v_y]
 
 def button_down(pos_mouse, ball):
     '''
     Обрабатывает нажатие левой кнопки мыши
     :param pos_mouse: кортеж из двух координат положения мыши
-    :param ball: список из абсциссыб ординаты и радиуса шарика
+    :param ball: список из абсциссы, ординаты и радиуса шарика
     '''
     k = 0
     if ((pos_mouse[0]-ball[0]) ** 2 + (pos_mouse[1]-ball[1]) ** 2) <= ball[2] ** 2:
         k = 1
     return k
+
+
+pygame.init()
+
+NAME = input("Введите ваше имя:  ")
+print(NAME, "добро пожаловать в игру. ")
+NUM = float(input("Введите желаемое количество шариков в секунду:  "))
+print("Результат (счет) в игре обратно пропорционален ее продолжительности")
+T = int(input("Введите желаемую длительность игры в секундах:  "))
+
+FPS = 30
+SIZE = WIDTH, HEIGHT = 500, 500 #Размер окошка
+screen = pygame.display.set_mode(SIZE)
     
 COLORS = const_colors()
-NUM = 0.1 #Число генерируемых шариков в секунду
-T = 30 #Длительность игры в секундахs
-TIME = 0 #Игра продоолжается, пока TIME < T * FPS
 
 clock = pygame.time.Clock()
 finished = False
+TIME = 0 #Игра продоолжается, пока TIME < T * FPS
 counter = 0 #Создаем счетчик, чтобы создавать шарик не при каждом обновлении экрана
-coords = [0, 0, 0, (0, 0, 0)] #Список из абсциссы, ординаты, радиуса шарика и его цвета
+coords = [(0, 0, 0), 0, 0, 0, 0, 0] #Список из абсциссы, ординаты, радиуса шарика и его цвета
 score = 0 #Переменная отвечает за количество пойманных шариков
-speed = [0.1 * WIDTH / FPS, 0.1 * HEIGHT / FPS] #смещение за 1/FPS секунды
 
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+        #Обработка нажатия на левую кнопку мыши
         elif (event.type == pygame.MOUSEBUTTONDOWN) and (event.button == 1):
-            score += button_down(event.pos, coords)
+            score += button_down(event.pos, coords[1:4:])
             counter = 0
-            print(score)
             
-    if counter == 0:
+    if counter == 0:# Создаем новый шарик, если старый уже давно бегает
         coords = new_ball()
        
-    screen.fill(COLORS[0])
-    coords, speed = move(coords, speed)
-
+    screen.fill(COLORS[0]) #Экран в черный
+    coords = move(coords) #Прорисовка сдвинутого шара
     pygame.display.update()
-    
     
     counter+=1 #Прошел 1 тик
     if counter > (FPS // NUM): #Если тиков достаточно -- обнуляем счетчик.
         counter = 0 #В следующий раз появится шарик)
+        
     TIME+=1
-    if TIME == T*FPS:
+    if TIME == T*FPS: #Завершение игры при окончании времени
         finished = True
-
+        
+score = int(score * 100 / T) #Нормализация счета. Далее -- запись в файл
+with open('output.txt', 'a') as file:
+    st = str(score) + ' ' + NAME + '\n'
+    file.write(str(score) + ' ' + NAME + '\n')
 pygame.quit()
